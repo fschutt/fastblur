49d62de081c1406d350f99f8904c2eee840e332e
Felix Schütt <felix.schuett@maps4print.com>
2017-09-15 10:25:35 +0200

    Release 0.1.0

--- src/test.rs
+++ src/test.rs
@@ -6,10 +6,21 @@ fn test_blur_image_correctly() {
     use super::utils;
 
     let image_bytes = include_bytes!("../assets/cballs.png");
-    if let Ok(image::DynamicImage::ImageRgb8(png_data)) = image::load_from_memory_with_format(image_bytes, image::ImageFormat::PNG) {
+    let image_reference_bytes = include_bytes!("../assets/cballs_reference_5px_blur.png");
+
+    let res1 = image::load_from_memory_with_format(image_bytes, image::ImageFormat::PNG);
+    let res2 = image::load_from_memory_with_format(image_reference_bytes, image::ImageFormat::PNG);
+
+    if let (Ok(image::DynamicImage::ImageRgb8(png_data)),
+            Ok(image::DynamicImage::ImageRgb8(reference_data))) =
+            (res1, res2)
+    {
         let width = png_data.width() as usize;
         let height = png_data.height() as usize;
         let data = png_data.into_raw();
+        let mut data_new = Vec::<[u8;3]>::with_capacity(width * height);
+        data_new.resize(width * height, [0, 0, 0]);
+
         let mut data_new = Vec::<[u8;3]>::with_capacity(width * height);
         data_new.resize(width * height, [0, 0, 0]);
 
@@ -20,10 +31,27 @@ fn test_blur_image_correctly() {
             }
         }
 
-        gaussian_blur(&mut data_new, width as usize, height as usize, 10.0);
+        let reference = reference_data.into_raw();
+        let mut reference_new = Vec::<[u8;3]>::with_capacity(width * height);
+        reference_new.resize(width * height, [0, 0, 0]);
+
+        for y in 0..height {
+            for x in 0..width {
+                let offset = ((width * y) + x) as usize;
+                reference_new[((width * y) + x) as usize] = [reference[offset * 3], reference[(offset * 3) + 1], reference[(offset * 3) + 2]];
+            }
+        }
+
+        gaussian_blur(&mut data_new, width as usize, height as usize, 6.0);
         utils::write_image("test.ppm", &data_new, width as usize, height as usize).unwrap();
 
-        assert!(true);
+        // this will fail, no matter what because the radius in the original algorithm isn't known
+        // and I don't know if Javascript does things differently in terms of floating-point calculations
+/*
+        for (idx, (px, other)) in data_new.iter().zip(reference_new.iter()).enumerate() {
+            if px != other { panic!("failed assertion @ byte {:?}", idx); }
+        }
+*/
     } else {
         panic!("could not decode png");
     }
